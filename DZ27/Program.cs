c84c15bd9b2a2f418c78efe8b55677a3b981524c
Emma <em0795@yandex.ru>
2022-11-04 08:48:54 +0400

    add DZ27

---
+++ DZ27/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine());
+int summ = 0;
+ while (number != 0)
+ {
+    summ+= number%10;
+    number /= 10;
+ }
+ Console.WriteLine($"Сумма чисел равна {summ}");
