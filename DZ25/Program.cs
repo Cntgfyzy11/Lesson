be1c6191646dd78293ef837954d2b7e7595a6356
Emma <em0795@yandex.ru>
2022-11-04 08:22:16 +0400

    add DZ25

---
+++ DZ25/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+Console.WriteLine("Введите число А:");
+Console.WriteLine("Введите число B:");
+ int numberA = int.Parse(Console.ReadLine());
+ int numberB = int.Parse(Console.ReadLine());
+ double c = Math.Pow(numberA, numberB);
+
+for (int i = 0; i <=numberA && i <=numberB; i++)
+
+Console.WriteLine($"Число {numberA} в степени {numberB} равно {c}");
